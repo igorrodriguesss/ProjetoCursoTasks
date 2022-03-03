718025c3513a730da048ea9fc3db13bd9275db89
Igor Rodrigues <igorr2693@gmail.com>
2022-03-03 15:13:39 -0300

    ProjetoFinalizado

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize('mvc', 'root', 'root', {
+const sequelize = new Sequelize('mvc', 'root', '', {
     host: 'localhost',
     dialect: 'mysql',
 })
